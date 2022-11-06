92e4b8cbe9482179455e2e27890bbb5e9eb8fcef
carloxx19 <carlosrtoledo.133@gmail.com>
2022-11-06 23:29:25 +0100

    Ejercicio base de datos

---
+++ base_datos/src/Main.java
@@ -0,0 +1,13 @@
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Metodos conexion = new Metodos("jdbc:mysql://localhost:8889/tienda?characterEconding=utf8", "root", "root");
+
+        conexion.driver("com.mysql.cj.jdbc.Driver"); // "com.mysql.cj.jdbc.Driver"
+        conexion.conectar();
+
+        conexion.menu();
+    }
+}
