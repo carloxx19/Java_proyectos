df54bfbcafa58ff4aa26ae96c820a02df9dc677b
carloxx19 <carlosrtoledo.133@gmail.com>
2022-11-11 12:28:30 +0100

    Ejercicio casino

--- Casino/src/Jugador.java
+++ Casino/src/Jugador.java
@@ -1,47 +1,11 @@
 public class Jugador {
-    public String nombre;
-    public int saldo;
-    public int apuesta;
+    public static int numeros = 0;
+    public int num;
+    public double dinero;
 
-
-    //Getter and Setter
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(int saldo) {
-        this.saldo = saldo;
-    }
-
-    public int getApuesta() {
-        return apuesta;
-    }
-
-    public void setApuesta(int apuesta) {
-        this.apuesta = apuesta;
-    }
-
-    //Constructor
-    public Jugador(int saldo) {
-        super();
-        this.nombre = nombre;
-        this.saldo = saldo;
-        this.apuesta = apuesta;
-    }
-
-    @Override
-    public String toString() {
-        return "Nombre del jugador: " + this.nombre +
-                "\nsaldo jugador: " + this.saldo +
-                "\napuesta jugador: " + this.apuesta+
-                "\n";
+    public Jugador() {
+        numeros++;
+        num = numeros;
+        this.dinero = 300;
     }
 }
